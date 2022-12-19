1eddf53eb27c1f5fe6d724dbcd1fa234fbee7a37
MustafaKaganSimsek <mustafakagan48@gmail.com>
2022-12-19 19:42:34 +0300

    asfsa

--- WarehouseManagementApplication/WebApp/src/app/app.module.ts
+++ WarehouseManagementApplication/WebApp/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserLoginComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
